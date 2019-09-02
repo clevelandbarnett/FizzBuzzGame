bcd6406e695aa8d5f4cfc63944d2490e16ed4657
Cleveland Barnett <51641336+clevelandbarnett@users.noreply.github.com>
2019-09-02 14:13:56 +0100

    FizzBuzz Game
    First attempt at writing FizzBuzz game in Python.

---
+++ FizzBuzzGame.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Triple speech marks are used to comment out lines of code
+
+"""
+
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+        
+#Hashes are good for commenting out things that are short
+        
